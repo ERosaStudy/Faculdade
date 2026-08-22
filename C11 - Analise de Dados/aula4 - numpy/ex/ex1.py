abba2011cedab32b96b80fe3c142b15b339d2807
Esthéfano <rosaesthefano@gmail.com>
2026-08-21 22:13:28 -0300

    exercicio 2

--- C11 - Analise de Dados/aula4 - numpy/ex/ex1.py
+++ C11 - Analise de Dados/aula4 - numpy/ex/ex1.py
@@ -7,6 +7,7 @@ np.random.seed()
 arr = np.ones(8)
 arr2 = np.random.randint(0,10,8)
 
+print(arr,arr2)
 arrSoma = arr + arr2
 ##print(f'Soma de indices: {arr + arr2}')
 
@@ -15,11 +16,36 @@ if arrSoma.sum() >= 40:
 else:
     mtz = arrSoma.reshape(2,4)
 
-print(mtz)
-
+print("--- Exercício 1 ---")
+print(f"Matriz resultante: \n", mtz)
+print('')
+print('')
 
 ## Exercicio 2
 
+print("\n--- Exercício 2 ---")
+tabuleiro = np.zeros([2, 2])
+
+bomba_lin = np.random.randint(0, 2)
+bomba_col = np.random.randint(0, 2)
+tabuleiro[bomba_lin, bomba_col] = 1
+
+jogadas_feitas = set()
+venceu = True
+
+for jogada in range(3):
+    print(f"\nJogada {jogada + 1}/3:")
+    lin = int(input("Escolha a linha (0 ou 1): "))
+    col = int(input("Escolha a coluna (0 ou 1): "))
+    
+    
+    if tabuleiro[lin, col] == 1:
+        print("Game Over!: (Try Again!")
+        venceu = False
+        break
+
+if venceu:
+    print("Congratulations! You beat the game! :)")
 
 ## Exercicio 3
 
