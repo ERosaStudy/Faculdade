5c1d462a15a365584d76f430c54ead80f80b2bc5
Esthéfano <rosaesthefano@gmail.com>
2026-08-21 22:20:06 -0300

    exercicio 3

--- C11 - Analise de Dados/aula4 - numpy/ex/ex1.py
+++ C11 - Analise de Dados/aula4 - numpy/ex/ex1.py
@@ -23,32 +23,49 @@ print('')
 
 ## Exercicio 2
 
-print("\n--- Exercício 2 ---")
-tabuleiro = np.zeros([2, 2])
+# print("\n--- Exercício 2 ---")
+# tabuleiro = np.zeros([2, 2])
 
-bomba_lin = np.random.randint(0, 2)
-bomba_col = np.random.randint(0, 2)
-tabuleiro[bomba_lin, bomba_col] = 1
+# bomba_lin = np.random.randint(0, 2)
+# bomba_col = np.random.randint(0, 2)
+# tabuleiro[bomba_lin, bomba_col] = 1
 
-jogadas_feitas = set()
-venceu = True
+# jogadas_feitas = set()
+# venceu = True
 
-for jogada in range(3):
-    print(f"\nJogada {jogada + 1}/3:")
-    lin = int(input("Escolha a linha (0 ou 1): "))
-    col = int(input("Escolha a coluna (0 ou 1): "))
+# for jogada in range(3):
+#     print(f"\nJogada {jogada + 1}/3:")
+#     lin = int(input("Escolha a linha (0 ou 1): "))
+#     col = int(input("Escolha a coluna (0 ou 1): "))
     
     
-    if tabuleiro[lin, col] == 1:
-        print("Game Over!: (Try Again!")
-        venceu = False
-        break
+#     if tabuleiro[lin, col] == 1:
+#         print("Game Over!: (Try Again!")
+#         venceu = False
+#         break
 
-if venceu:
-    print("Congratulations! You beat the game! :)")
+# if venceu:
+#     print("Congratulations! You beat the game! :)")
 
 ## Exercicio 3
 
+print("\n--- Exercício 3 ---")
+
+num_lin = np.random.randint(0, 7)
+num_col = np.random.randint(0, 7)
+
+mtz_ex3 = np.random.randint(1, 10, (num_lin, num_col))
+
+linhas, colunas = mtz_ex3.shape
+total_elementos = linhas * colunas
+
+if total_elementos % 2 == 0:
+    tipo = "par" 
+else: 
+    tipo = "ímpar"
+
+print(f"Dimensões: {linhas}x{colunas} | Total: {total_elementos} elementos")
+print(f"Poderia se tornar um vetor unidimensional com número {tipo} de elementos.")
 
 ## Exercicio 4
 
