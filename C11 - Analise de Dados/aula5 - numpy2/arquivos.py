3263beefceea6e7b0d32806cfa59a11464cda3ad
Esthéfano <rosaesthefano@gmail.com>
2026-08-27 20:40:09 -0300

    update arquivos

--- C11 - Analise de Dados/aula5 - numpy2/arquivos.py
+++ C11 - Analise de Dados/aula5 - numpy2/arquivos.py
@@ -1,5 +1,18 @@
-import numpy as n
+import numpy as np
 
 ##Importando arquivos de teste
 
-dataset = np.loadtxt('numpy.txt')
+dataset = np.loadtxt('numpy.txt', delimiter=',',dtype=str)
+
+print(f'TXT:{dataset}')
+print('')
+
+##criando arquivos puxando informações de outro
+
+##np.savetxt('numpy2.txt',dataset, fmt='%s')
+
+np.save('Arquivo.npy', dataset)
+
+dataset1 = np.load('Arquivo.npy')
+
+print(f'NPY:{dataset}')
